360eb15b847f3eac52ce08217ffe076b621718fc
svetlanablagojevic <svetlana.sege@gmail.com>
2017-02-03 19:07:12 +0100

    Add files via upload

--- src/main/java/com/teamtreehouse/instateam/web/controller/RoleController.java
+++ src/main/java/com/teamtreehouse/instateam/web/controller/RoleController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
-import java.util.List;
 
 /**
  * Created by GoranB on 2017-01-26.
@@ -21,90 +20,73 @@ import java.util.List;
 @Controller
 public class RoleController {
 
+
     @Autowired
     private RoleService roleService;
 
-    // index of all categories
-    @SuppressWarnings("unchecked")
+
     @RequestMapping("/roles")
-    public String listRoles(Model model) {
-        // Get all roles
-        List<Role> roles = roleService.findAll();
-        // add roles to model
-        model.addAttribute("roles", roles);
-        // if user typed wrong data it will be shown
-        if (!model.containsAttribute("role")) {
-            // add empty newRole so that we can fill it with
-            // data, when making POST request on this page
+    public String listRoles(Model model){
+        if(!model.containsAttribute("role")){
             model.addAttribute("role", new Role());
         }
+        model.addAttribute("roles", roleService.findAll());
         return "role/index";
     }
 
-    // Form for adding new role
+
     @RequestMapping(value = "/add-role", method = RequestMethod.POST)
-    public String addNewRole(@Valid Role role,BindingResult result,RedirectAttributes redirectAttributes) {
-        if (result.hasErrors()) {
-            System.out.println(result);
-            redirectAttributes.addFlashAttribute(
-                    "org.springframework.validation.BindingResult.newRole", result);
+    public String addRole(@Valid Role role, BindingResult result, RedirectAttributes redirectAttributes){
+        if(result.hasErrors()){
+            redirectAttributes.addFlashAttribute("flash",
+                    new FlashMessage("The name you have used for the role is invalid. Please try again.",
+                            FlashMessage.Status.FAILURE));
             redirectAttributes.addFlashAttribute("role", role);
             return "redirect:/roles";
         }
-
-        // saveOrUpdate role to database
         roleService.save(role);
-
-        redirectAttributes.addFlashAttribute("flash", new FlashMessage("New Role: '" + role.getName() +
-                "' was successfully added!",
+        redirectAttributes.addFlashAttribute("flash", new FlashMessage(String.format("Role '%s' has been successfully added.", role.getName()),
                 FlashMessage.Status.SUCCESS));
-        // redirect back to same page
-        return "redirect:/roles";
-
-     }
-
-    @RequestMapping(value = "/role/{id}")
-    public String editRole(Model model, @PathVariable Long id) {
-        Role role= roleService.findById(id);
-        if (!model.containsAttribute("role")){
-            model.addAttribute("role", role);
-        }
-
-        return "/role/edit";
-    }
-
-    //update role
-    @RequestMapping(value = "/role/update", method = RequestMethod.POST)
-    public String updateRole(@Valid Role role, BindingResult result, RedirectAttributes redirectAttributes) {
-        if (result.hasErrors()) {
-
-            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.role", result);
-            redirectAttributes.addFlashAttribute("role", role);
-            return String.format("redirect:/role/%s", role.getId());
-        }
-
-        roleService.save(role);
-        redirectAttributes.addFlashAttribute("flash", new FlashMessage("Role:'" + role.getName() + "'updated!", FlashMessage.Status.SUCCESS));
         return "redirect:/roles";
     }
 
-  //delete role
 
-    @RequestMapping(value = "/roles/{roleId}/delete")
-    public String deleteRole(@PathVariable Long roleId, RedirectAttributes redirectAttributes) {
-        //find role by id
+    @RequestMapping(value = "/roles/{roleId}/detail")
+    public String roleDetail(@PathVariable Long roleId, Model model){
         Role role = roleService.findById(roleId);
-        // if not found throw not found exception
-        if (role == null) {
-            //throw new NotFoundException("Role not Found");
-            redirectAttributes.addFlashAttribute("flash", new FlashMessage("Role not found!", FlashMessage.Status.FAILURE));
+        model.addAttribute("role", role);
+        return "role/detail";
+    }
+
+
+
+    //  edit role
+    @RequestMapping(value = "/roles/{roleId}/edit", method = RequestMethod.POST)
+    public String editRole(@Valid Role role, @PathVariable Long roleId,BindingResult result, RedirectAttributes redirectAttributes){
+        if(result.hasErrors()){
+            redirectAttributes.addFlashAttribute("flash", new FlashMessage("The name you have used for the role is invalid. Please try again.",
+                    FlashMessage.Status.FAILURE));
+            return String.format("redirect:/roles/%s/detail", role.getId());
         }
-        // delete from database
-        roleService.delete(role);
-        // success flash message
-        redirectAttributes.addFlashAttribute("flash", new FlashMessage("Role: '" + role.getName() + "' was successfully deleted!", FlashMessage.Status.SUCCESS));
-        // redirect back to page with roles
+        String oldName = roleService.findById(roleId).getName();
+        roleService.save(role);
+        String newName = role.getName();
+
+        redirectAttributes.addFlashAttribute("flash", new FlashMessage(String.format("Role '%s' has been renamed to '%s'.", oldName, newName) ,
+                FlashMessage.Status.SUCCESS));
+
+
         return "redirect:/roles";
     }
 
+
+    @RequestMapping(value = "/roles/{roleId}/delete", method = RequestMethod.POST)
+    public String deleteRole(@PathVariable Long roleId, RedirectAttributes redirectAttributes){
+        Role role = roleService.findById(roleId);
+        redirectAttributes.addFlashAttribute("flash", new FlashMessage(String.format("Role '%s' has been deleted successfuly.",
+                role.getName()),
+                FlashMessage.Status.SUCCESS));
+        roleService.delete(role);
+        return "redirect:/roles";
+    }
 }
